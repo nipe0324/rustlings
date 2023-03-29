6d37daefb2acde975c901f45759263d1506046ee
Shojiro Yanagisawa <nipe0324@gmail.com>
2023-03-30 00:18:41 +0900

    solve genericas, iterators, lifetimes, tests and traits

--- exercises/iterators/iterators4.rs
+++ exercises/iterators/iterators4.rs
@@ -1,8 +1,6 @@
 // iterators4.rs
 // Execute `rustlings hint iterators4` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
-
 pub fn factorial(num: u64) -> u64 {
     // Complete this function to return the factorial of num
     // Do not use:
@@ -13,7 +11,13 @@ pub fn factorial(num: u64) -> u64 {
     // For an extra challenge, don't use:
     // - recursion
     // Execute `rustlings hint iterators4` for hints.
+    let v: Vec<u64> = (1..=num).collect();
+    v.iter().fold(1, |sum, x| sum * x)
 }
+
+// let v = vec![1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+// let sum = v.iter().fold(0, |sum, x| sum * x);
+// println!("{}", sum);  // 55
 
 #[cfg(test)]
 mod tests {
