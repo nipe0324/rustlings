127a88c0335813917f07f5545a470515f4c5edea
Shojiro Yanagisawa <nipe0324@gmail.com>
2023-04-02 21:17:31 +0900

    finish rustlings

--- exercises/conversions/from_into.rs
+++ exercises/conversions/from_into.rs
@@ -35,10 +35,27 @@ impl Default for Person {
 // If while parsing the age, something goes wrong, then return the default of Person
 // Otherwise, then return an instantiated Person object with the results
 
-// I AM NOT DONE
-
 impl From<&str> for Person {
     fn from(s: &str) -> Person {
+        if s.len() == 0 {
+            return Person::default();
+        }
+        let mut split = s.split(",");
+        if split.clone().count() != 2 {
+            return Person::default();
+        }
+        let name = split.next().unwrap();
+        if name.len() == 0 {
+            return Person::default();
+        }
+        let age = split.next().unwrap().parse::<usize>();
+        match age {
+            Ok(age) => Person {
+                name: String::from(name),
+                age,
+            },
+            Err(_) => Person::default(),
+        }
     }
 }
 
